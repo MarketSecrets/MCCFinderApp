43121fc1e5415312b467252348db0b925f41dd7f
MarketSecrets <165997162+MarketSecrets@users.noreply.github.com>
2025-03-20 15:18:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,16 +13,44 @@ function domReady(fn) {
 
 domReady(function () {
 
+ // Function to fetch and parse the JSON file
+    function fetchMccData() {
+        return fetch('mcc.json')
+            .then(response => response.json())
+            .then(data => {
+                return data.data;
+            })
+            .catch(error => {
+                console.error('Error fetching MCC data:', error);
+            });
+    }
+
     // If found you qr code
     function onScanSuccess(decodeText, decodeResult) {
         parsed_url = new URL(decodeText);
         query_params = Object.fromEntries(new URLSearchParams(parsed_url.search));
         mc_value = query_params.mc || null;
         alert("MCC is : " + mc_value);
+        //if(mc_value){}
+
+
 
         //alert("You Qr is : " + decodeText, decodeResult);
 
-    }
+        if(mc_value){
+        // Fetch MCC data and use it
+        fetchMccData().then(mccData => {
+            if (mccData) {
+                const mccEntry = mccData.find(entry => entry['MCC Code'] == mc_value);
+                if (mccEntry) {
+                    alert("MCC Classification: " + mccEntry.Classification);
+                } else {
+                    alert("MCC Code not found");
+                }
+            }
+        });
+        }
+        }
 
     let htmlscanner = new Html5QrcodeScanner(
         "my-qr-reader",
